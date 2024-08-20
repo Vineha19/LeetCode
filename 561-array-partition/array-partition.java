05233b3e721719b50666e8b87b556f11b13a7928
Vineha19 <146471545+Vineha19@users.noreply.github.com>
2024-08-20 23:11:05 +0530

    Time: 12 ms (96.45%) | Memory: 46.9 MB (13.49%) - LeetSync

---
+++ 561-array-partition/array-partition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+      Arrays.sort(nums);
+      int a=0;
+      for(int i=0;i<nums.length;i+=2){
+        a+=nums[i];
+      }
+      return a;
+        
+    }
+}
